86670e40b67db2b3716d1e2330045f11a1347e4e
Tarun Reddy <tarunreddy18.vk@gmail.com>
2022-03-31 09:24:57 +0530

    second commit

---
+++ passport/google.js
@@ -0,0 +1,47 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/user');
+const keys = require('../config/keys');
+
+passport.serializeUser((user,done) => {
+    return done(null,user.id);
+});
+
+passport.deserializeUser((id,done) => {
+    User.findById(id,(err,user) => {
+        return done(err,user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: keys.GoogleClientID,
+    clientSecret: keys.GoogleClientSecret,
+    callbackURL: 'https://localhost:3000/auth/google/callback'
+},(accessToken, refreshToken, profile, done) => {
+    console.log(profile);
+
+    User.findOne({google:profile.id},(err,user) => {
+        if(err) {
+            return done(err);
+        }
+        if (user) {
+            return done(null,user);
+        }else{
+            const newUser = {
+                firstname: profile.name.givenName,
+                lastname: profile.name.familyName,
+                image: profile.photos[0].value,
+                fullname: profile.displayName,
+                google: profile.id
+            }
+            new User(newUser).save((err,user) => {
+                if (err) {
+                    return done(err);
+                }
+                if (user) {
+                    return done(null,user);
+                }
+            });
+        }
+    });
+}));
