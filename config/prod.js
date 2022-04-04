d94bb298fb494d37cba06e57d863f3d8bf17d059
Tarun Reddy <tarunreddy18.vk@gmail.com>
2022-04-04 08:38:25 +0530

    mongoDB link updated

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoDB: 'mongodb+srv://datingapp:datingapp@datingsite.rrxzv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    MongoDB: process.env.MONGO_DB,
     FacebookAppID: '3033565500230081',
     FacebookAppSecret:'0e08d1ee57058ac9275daba7ff48b173',
     GoogleClientID: '662417253750-8qb0vljn9p7iuht7umh3fssg2gpgb8u9.apps.googleusercontent.com',
