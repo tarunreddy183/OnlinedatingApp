912a6a75ac3dd479c2d22ad423394cb11f711981
Tarun Reddy <tarunreddy18.vk@gmail.com>
2022-04-04 07:30:51 +0530

    push changes to heroku

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MongoDB: 'mongodb+srv://datingapp:datingapp@datingsite.rrxzv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    FacebookAppID: '3033565500230081',
+    FacebookAppSecret:'0e08d1ee57058ac9275daba7ff48b173',
+    GoogleClientID: '662417253750-8qb0vljn9p7iuht7umh3fssg2gpgb8u9.apps.googleusercontent.com',
+    GoogleClientSecret: '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'
+}
