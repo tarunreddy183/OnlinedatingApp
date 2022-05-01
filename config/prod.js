b58e947661f222dda531b938dcbff739cbf8c77f
Gophi Tarun Reddy Vippala <53594052+tarunreddy183@users.noreply.github.com>
2022-05-01 15:39:31 +0530

    Delete prod.js

--- config/prod.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    MongoDB: process.env.MONGO_DB,
-    FacebookAppID: process.env.FB_APP_ID,
-    FacebookAppSecret: process.env.FB_APP_SECRET,
-    GoogleClientID: '662417253750-8qb0vljn9p7iuht7umh3fssg2gpgb8u9.apps.googleusercontent.com',
-    GoogleClientSecret: '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',
-    AWSAccessKeyID: process.env.AWS_ACCESS_KEY,
-    AWSAccessKeySecret: process.env.AWS_ACCESS_SECRET
-}
